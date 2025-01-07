15b61ad487e8ad8eeec996b90f14bffe1686c759
Muhammadyusuf <mukhammadyusuf.alijanov25@gmail.com>
2025-01-07 11:16:41 +0500

    Implement dynamic search

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.db.models import Q
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -15,44 +17,59 @@ class PostViewSet(ModelViewSet):
     filter_backends = [SearchFilter] # DRF built-in filter customization class, SearchFilter class is used to filter customize depending on URL (?search=)
     search_fields = ['title', 'description'] # required by DRF built-in filter to point which fields in database are need to be searched
 
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        search_term = self.request.query_params.get("search", None)
+        
+        if search_term:
+            queryset = queryset.filter(Q(title__icontains=search_term) | Q(description__icontains = search_term))   
+            return queryset
+
+        return queryset
     
     def list(self, request, *args, **kwargs):
         '''
         Custom list function to get the list of Posts
         '''
-        queryset = self.filter_queryset(self.get_queryset()) # custom queryset referencing to filter_backends
-        paginator = PageNumberPagination() # DRF built-in paginator class
-        paginator.page_size = 20 # Number of queryset instances that must be displayed
-        page = paginator.paginate_queryset(queryset, request)
+        queryset = self.filter_queryset(self.get_queryset())  # Apply filters
+        paginator = PageNumberPagination()  # DRF paginator
+        paginator.page_size = 20
+        page = paginator.paginate_queryset(queryset, request)  # Paginated queryset
 
-        page = self.paginate_queryset(queryset) # returns the instances of queryset for one page
-        if page is not None: # check if it is possible to paginate, is there enough objects in queryset
+        # Check if the request is an AJAX call
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            # Return JSON response for AJAX calls
+            serializer = self.get_serializer(page if page else queryset, many=True, context={
+                'paginator':paginator,
+                'page_obj': paginator.page
+            })
+            return Response(serializer.data)
+
+        # For regular requests, render the HTML template
+        if page is not None:  # Paginate if possible
             serializer = self.get_serializer(page, many=True)
             context = {
-                'posts':serializer.data,
-                'paginator':paginator,
-                'page_obj':paginator.page,
+                'posts': serializer.data,
+                'paginator': paginator,
+                'page_obj': paginator.page,
             }
-            return render(request, 'index.html', context=context)
+            return render(request, 'index.html', context)
 
-        
+        # Render without pagination
         serializer = self.get_serializer(queryset, many=True)
         context = {
-            'posts':serializer.data,
+            'posts': serializer.data,
         }
-
-        return render(request, 'index.html', context=context)
+        return render(request, 'index.html', context)
                 
     def retrieve(self, request, *args, **kwargs):
         '''
         Customer retrieve function to get the post details
         '''
         post = self.get_object() # get single object from database
-        
         return render(request, 'post_detail.html', {
             'post':post
         })
-
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
