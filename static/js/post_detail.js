36367b16fc746723a9138bec814f4ae06724ba93
Muhammadyusuf <muhammadyusuf.alijonov@auto.uz>
2025-01-18 17:13:56 +0500

    fix: fix bug with adding delete post feature

--- static/js/post_detail.js
+++ static/js/post_detail.js
@@ -12,7 +12,6 @@ document.getElementById("back-btn").addEventListener("click", function () {
 
 document.getElementById("delete-btn").addEventListener("click", function () {
     deleteRequest();
-    
 });
 
 document.addEventListener("DOMContentLoaded", () => {
